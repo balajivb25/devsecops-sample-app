fefd7da09c9bfec3ef856ce2f1645aae8f004f50
balajivb25 <balajiv.b25@gmail.com>
2025-05-05 19:47:47 +1000

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api')
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return <h1>{message}</h1>;
+}
+
+export default App;
